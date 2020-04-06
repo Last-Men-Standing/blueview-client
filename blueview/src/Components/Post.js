6e8e1f8733dbfab03195d7ce679c67debf498584
allenz-0 <allenz@rpi.edu>
2020-04-06 13:42:01 -0400

    Updated to use props and removed extraneous code

--- blueview/src/Components/Post.js
+++ blueview/src/Components/Post.js
@@ -6,31 +6,17 @@ import './Post.css';
 class Post extends React.Component {
   constructor(props) {
     super(props);
+    /*
     this.state = {
       user: 'Rachel Beenest', 
       date: '2020-05-17',
       title: 'Post Title',
       text: 'The police officer the police officer the police officer the police officer the police officer the police officer the police officer the police officer the police officer the police officer the police officer the police officer the police officer the police officer the police officer the police officer the police officer the police officer the police officer the police officer the police officer the police officer.'
     }
-
-    this.updateUsername = this.updateUsername.bind(this);
-    this.updatePassword = this.updatePassword.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    */
   }
 
-  updateUsername(event) {
-    this.setState({username: event.target.value});
-  }
-
-  updatePassword(event) {
-    this.setState({password: event.target.value});
-  }
-
-  handleSubmit(event) {
-    alert('Username: ' + this.state.username + "\nPassword: " + this.state.password);
-    event.preventDefault();
-  }
-
+  /* VERSION USING STATE
   render() {
     return (
       <div className="postBox">
@@ -40,6 +26,21 @@ class Post extends React.Component {
         </p>
         <p className="postBody">
           {this.state.text}
+        </p>
+      </div>
+    );
+  }
+  */
+
+  render() {
+    return (
+      <div className="postBox">
+        <p className="postInfo">
+          <span className="postTitle">{this.props.title}</span> on {this.props.date}
+          <a className="userName">{this.props.user}</a>
+        </p>
+        <p className="postBody">
+          {this.props.text}
         </p>
       </div>
     );
